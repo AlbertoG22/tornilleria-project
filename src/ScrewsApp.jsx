80610391493aff4bc979c2474a8d73d35e6b2983
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-10-30 17:51:08 -0600

    MUI configuration with Vite

--- src/ScrewsApp.jsx
+++ src/ScrewsApp.jsx
@@ -1,9 +1,10 @@
 import { AppRouter } from './router/AppRouter';
+import { AppTheme } from './theme';
 
 export const ScrewsApp = () => {
   return (
-    <>
+    <AppTheme>
       <AppRouter />
-    </>
+    </AppTheme>
   );
 };
--- src/dashboard/pages/HomePage.jsx
+++ src/dashboard/pages/HomePage.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import { Typography } from '@mui/material';
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Typography variant='h1'>HomePage</Typography>
   );
 };
