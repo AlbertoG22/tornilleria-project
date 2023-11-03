20e53f285fb8382c02d8dd6552dfbd473d7da1db
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-11-02 20:13:50 -0600

    search field mui

--- src/dashboard/pages/HomePage.jsx
+++ src/dashboard/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { Typography } from '@mui/material';
 import { DashboardLayout } from '../layout/dashboardLayout';
 
 export const HomePage = () => {
+
   return (
     <DashboardLayout>
       <Typography variant='h6'>Cotizaciones</Typography>
--- src/dashboard/pages/SearchPage.jsx
+++ src/dashboard/pages/SearchPage.jsx
@@ -1,10 +1,12 @@
+import { SearchField } from '../components/SearchField';
 import { DashboardLayout } from '../layout/dashboardLayout';
 import { Typography } from '@mui/material';
 
 export const SearchPage = () => {
   return (
     <DashboardLayout>
-      <Typography variant='h6'>Herramienta Urrea</Typography>
+      {/* <Typography variant='h6'>Herramienta Urrea</Typography> */}
+      <SearchField />
     </DashboardLayout>
   );
 };
