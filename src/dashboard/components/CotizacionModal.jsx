aa7ce478b9533d3f312063396cd6189b2798e3e9
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-11-22 19:53:32 -0600

    modal scrollable and center form of customer name

--- src/dashboard/components/CotizacionModal.jsx
+++ src/dashboard/components/CotizacionModal.jsx
@@ -1,18 +1,32 @@
 import Modal from 'react-modal';
 import { CotizacionForm } from './';
+import { Box, FormControl, FormHelperText, FormLabel, Grid, Input } from '@mui/material';
 
 const customStyles = {
   overlay: {
     zIndex: 9999,
   },
   content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
+    top: "8%",
+    left: "5%",
+    right: "5%",
+    bottom: "8%",
+    // marginRight: "-45%",
+    // transform: "translate(-50%, -50%)",
     zIndex: 10000,
+
+    // position: 'absolute',
+    // top: '40px',
+    // left: '40px',
+    // right: '40px',
+    // bottom: '40px',
+    border: '1px solid #ccc',
+    background: '#fff',
+    overflow: 'auto',
+    // WebkitOverflowScrolling: 'touch',
+    borderRadius: '4px',
+    // outline: 'none',
+    // padding: '20px'
   },
 };
 
@@ -25,9 +39,40 @@ export const CotizacionModal = ({ isModalOpen, onCloseModal }) => {
       onRequestClose={onCloseModal}
       style={customStyles}
     >
-      <h1>Nueva Cotización</h1>
-      <hr />
-      <CotizacionForm />
+      <Grid container spacing={2} sx={{ mt: 1 }}>
+        <Grid item xs={12} sx={{ textAlign: 'center' }}>
+          <h1>Nueva Cotización</h1>
+          {/* <hr /> */}
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            mb: 6
+            // backgroundColor: 'red',
+          }}
+          // sx={{ backgroundColor: 'red' }}
+        >
+          <Box sx={{
+            // backgroundColor: 'blue',
+            width: '80%' 
+          }}>
+            <FormControl
+              sx={{ width: '100%' }}
+            >
+              <FormLabel>Cliente</FormLabel>
+              <Input placeholder="Nombre" />
+              {/* <FormHelperText>This is a helper text.</FormHelperText> */}
+            </FormControl>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <CotizacionForm />
+        </Grid>
+      </Grid>
     </Modal>
   );
 };
